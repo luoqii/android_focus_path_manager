884d1253a81b640820917a30197d64bffcec1f75
bysong <bysong@tudou.com>
2016-05-09 11:49:12 +0800

    sleep 1s

--- app/src/androidTest/java/org/bb/android/focuspathmanager/ApplicationTest.java
+++ app/src/androidTest/java/org/bb/android/focuspathmanager/ApplicationTest.java
@@ -7,7 +7,7 @@ import android.test.ApplicationTestCase;
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
-    public static final long SLEEP_AFTER_KEY = 500;
+    public static final long SLEEP_AFTER_KEY = 1000;
 
     public ApplicationTest() {
         super(Application.class);
